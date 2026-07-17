3f1430b94287abc225d9c351e54cea6af7c47d40
Shashank <shashankb150@gmail.com>
2026-07-17 23:51:34 +0530

    Learn Number data type

---
+++ Datatypes/constDemo.js
@@ -0,0 +1,22 @@
+const PI=3.14
+//PI=2;
+console.log(PI);
+
+const data ={name:"jatin"};
+console.log(data);
+console.log(data.name);
+
+data.name='katrina';
+console.log(data);
+console.log(data.name)
+
+//data={name:'shashank'};
+//console.log(data.name)
+
+const array=[10,20,40];
+console.log(array);
+
+array[2]=30;
+console.log(array);
+
+
